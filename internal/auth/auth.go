3bdff8cb869e76108e3b9387be8cdcc07aa31ec6
Serg K <kozunov@example.com>
2025-11-20 21:46:30 +0300

    refactoring

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/serg2014/go-goph-keeper/internal/models"
 )
 
@@ -56,70 +55,4 @@ func sign(value, key []byte) string {
 
 func SignPassword(password string) string {
 	return sign([]byte(password), []byte(secretForPassword))
-}
-
-// Claims — структура утверждений, которая включает стандартные утверждения
-// и одно пользовательское — UserID
-type Claims struct {
-	jwt.RegisteredClaims
-	UserID    *models.UserID
-	IsRefresh bool
-}
-
-// BuildAccessJWT создаёт access токен и возвращает его в виде строки.
-func BuildAccessJWT(userID *models.UserID) (string, error) {
-	return buildJWT(userID, accessTokenExpire, false)
-}
-
-// BuildAccessJWT создаёт refresh токен и возвращает его в виде строки.
-func BuildRefreshJWT(userID *models.UserID) (string, error) {
-	return buildJWT(userID, refreshTokenExpire, true)
-}
-
-func buildJWT(userID *models.UserID, expire time.Duration, refresh bool) (string, error) {
-	// создаём новый токен с алгоритмом подписи HS256 и утверждениями — Claims
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
-		RegisteredClaims: jwt.RegisteredClaims{
-			// когда токен перестанет быть валидным
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expire)),
-		},
-		// собственное утверждение
-		UserID:    userID,
-		IsRefresh: refresh,
-	})
-
-	// создаём строку токена
-	tokenString, err := token.SignedString([]byte(secretForToken))
-	if err != nil {
-		return "", err
-	}
-
-	// возвращаем строку токена
-	return tokenString, nil
-}
-
-func GetUserIDFromToken(tokenString string) (*models.UserID, bool, error) {
-	// создаём экземпляр структуры с утверждениями
-	claims := &Claims{}
-	// парсим из строки токена tokenString в структуру claims
-	token, err := jwt.ParseWithClaims(
-		tokenString,
-		claims,
-		func(t *jwt.Token) (any, error) {
-			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("%w: %v", ErrTokenBadSigningMethod, t.Header["alg"])
-			}
-			return []byte(secretForToken), nil
-		})
-
-	if token.Valid {
-		// возвращаем ID пользователя в читаемом виде
-		return claims.UserID, claims.IsRefresh, nil
-	}
-
-	if errors.Is(err, jwt.ErrTokenExpired) {
-		return nil, claims.IsRefresh, ErrTokenExpired
-	}
-
-	return nil, claims.IsRefresh, err
 }
---
+++ internal/auth/jwt.go
@@ -0,0 +1,76 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/serg2014/go-goph-keeper/internal/models"
+)
+
+// Claims — структура утверждений, которая включает стандартные утверждения
+// и одно пользовательское — UserID
+type Claims struct {
+	jwt.RegisteredClaims
+	UserID    *models.UserID
+	IsRefresh bool
+}
+
+// BuildAccessJWT создаёт access токен и возвращает его в виде строки.
+func BuildAccessJWT(userID *models.UserID) (string, error) {
+	return buildJWT(userID, accessTokenExpire, false)
+}
+
+// BuildAccessJWT создаёт refresh токен и возвращает его в виде строки.
+func BuildRefreshJWT(userID *models.UserID) (string, error) {
+	return buildJWT(userID, refreshTokenExpire, true)
+}
+
+func buildJWT(userID *models.UserID, expire time.Duration, refresh bool) (string, error) {
+	// создаём новый токен с алгоритмом подписи HS256 и утверждениями — Claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			// когда токен перестанет быть валидным
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expire)),
+		},
+		// собственное утверждение
+		UserID:    userID,
+		IsRefresh: refresh,
+	})
+
+	// создаём строку токена
+	tokenString, err := token.SignedString([]byte(secretForToken))
+	if err != nil {
+		return "", err
+	}
+
+	// возвращаем строку токена
+	return tokenString, nil
+}
+
+func GetUserIDFromToken(tokenString string) (*models.UserID, bool, error) {
+	// создаём экземпляр структуры с утверждениями
+	claims := &Claims{}
+	// парсим из строки токена tokenString в структуру claims
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		claims,
+		func(t *jwt.Token) (any, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("%w: %v", ErrTokenBadSigningMethod, t.Header["alg"])
+			}
+			return []byte(secretForToken), nil
+		})
+
+	if token.Valid {
+		// возвращаем ID пользователя в читаемом виде
+		return claims.UserID, claims.IsRefresh, nil
+	}
+
+	if errors.Is(err, jwt.ErrTokenExpired) {
+		return nil, claims.IsRefresh, ErrTokenExpired
+	}
+
+	return nil, claims.IsRefresh, err
+}
