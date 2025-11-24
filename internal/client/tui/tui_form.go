b927423d8aa77d5d46fb6ac6da1d36d25bd2631b
Serg K <kozunov@example.com>
2025-11-24 13:39:39 +0300

    refactoring

--- internal/client/tui/tui_form.go
+++ internal/client/tui/tui_form.go
@@ -22,165 +22,6 @@ var (
 	ErrMetaName      = fmt.Errorf("bad key %s", models.MetaKeyName)
 )
 
-func tuiMeta(meta models.Meta) *huh.Text {
-	metaStr, err := meta.PrettyString()
-	if err != nil {
-		// TODO залогировать ошибку
-	}
-	return huh.NewText().
-		Title("Meta info").
-		Description("json dict format. Key __name__ is not allowed to be used.").
-		Validate(func(data string) error {
-			clear(meta)
-			if data == "" {
-				return nil
-			}
-			err := json.Unmarshal([]byte(data), &meta)
-			if err != nil {
-				return fmt.Errorf("%w: %w", ErrNotDict, err)
-			}
-			if _, ok := meta[models.MetaKeyName]; ok {
-				return ErrMetaName
-			}
-			return nil
-		}).
-		Value(&metaStr).
-		Key("meta")
-}
-
-func secretNameInput(name *string) *huh.Input {
-	return huh.NewInput().
-		Title("Secret name").
-		Key("name").
-		Validate(func(data string) error {
-			if data == "" {
-				return ErrRequiredField
-			}
-			return nil
-		}).
-		Value(name)
-}
-
-func tuiFormCredirCard(secret *models.Secret, save *bool) *huh.Form {
-	return huh.NewForm(
-		huh.NewGroup(
-			secretNameInput(&secret.Name),
-			huh.NewInput().
-				Title("Card number").
-				Key("card_number").
-				CharLimit(16).
-				Validate(func(data string) error {
-					if data == "" {
-						return ErrRequiredField
-					}
-					if len(data) != 16 {
-						return ErrCardLength
-					}
-					_, err := strconv.ParseInt(data, 10, 64)
-					if err != nil {
-						return ErrOnlyDigits
-					}
-					secret.Data.CreditCard.Number = data
-					return nil
-				}).
-				Value(&secret.Data.CreditCard.Number),
-			huh.NewInput().
-				Title("Exp").
-				Description("MM/YY").
-				Key("card_expire").
-				CharLimit(5).
-				Validate(func(data string) error {
-					if data == "" {
-						return ErrRequiredField
-					}
-
-					if len(data) != 5 {
-						return ErrExpireFormat
-					}
-					parts := strings.Split(data, "/")
-					if len(parts) != 2 {
-						return ErrExpireFormat
-					}
-					i, err := strconv.ParseInt(parts[0], 10, 32)
-					if err != nil {
-						return ErrOnlyDigits
-					}
-					if i < 1 || i > 12 {
-						return ErrCardMonth
-					}
-					_, err = strconv.ParseInt(parts[1], 10, 32)
-					if err != nil {
-						return ErrOnlyDigits
-					}
-
-					secret.Data.CreditCard.Exp = data
-					return nil
-				}).
-				Value(&secret.Data.CreditCard.Exp),
-			huh.NewInput().
-				Key("card_cvv").
-				Title("cvv").
-				CharLimit(3).
-				Validate(func(data string) error {
-					if data == "" {
-						return ErrRequiredField
-					}
-					if len(data) != 3 {
-						return ErrCardCvvLength
-					}
-					_, err := strconv.ParseInt(data, 10, 32)
-					if err != nil {
-						return ErrOnlyDigits
-					}
-					secret.Data.CreditCard.Cvv = data
-					return nil
-				}).
-				Value(&secret.Data.CreditCard.Cvv),
-			tuiMeta(secret.Meta),
-			tuiComfirm(secret, save),
-		),
-		tuiConfirmDelete(secret, save),
-	)
-}
-
-func tuiFormLoginPassword(secret *models.Secret, save *bool) *huh.Form {
-	return huh.NewForm(
-		huh.NewGroup(
-			secretNameInput(&secret.Name),
-			huh.NewInput().
-				Key("login").
-				Title("Login").
-				Validate(func(data string) error {
-					if data == "" {
-						return ErrRequiredField
-					}
-					return nil
-				}).
-				Value(&secret.Data.LoginPassword.Login),
-			huh.NewInput().
-				Key("password").
-				Title("Password").
-				Validate(func(data string) error {
-					if data == "" {
-						return ErrRequiredField
-					}
-					return nil
-				}).
-				Value(&secret.Data.LoginPassword.Password),
-			tuiMeta(secret.Meta),
-			tuiComfirm(secret, save),
-		),
-		tuiConfirmDelete(secret, save),
-	)
-}
-
-func tuiComfirm(secret *models.Secret, save *bool) *huh.Confirm {
-	return huh.NewConfirm().
-		Title("Save changes?").
-		Key("save").
-		Value(save)
-}
-
 func tuiFormAddOrEditSecret(secret *models.Secret, save *bool, fn func(*models.Secret) (string, error)) (*huh.Form, error) {
 	var form *huh.Form
 	switch secret.Type {
@@ -205,31 +46,128 @@ func tuiFormAddOrEditSecret(secret *models.Secret, save *bool, fn func(*models.S
 	return form, nil
 }
 
+func tuiFormLoginPassword(secret *models.Secret, save *bool) *huh.Form {
+	opts := []huh.Field{
+		huh.NewInput().
+			Key("login").
+			Title("Login").
+			Validate(func(data string) error {
+				if data == "" {
+					return ErrRequiredField
+				}
+				return nil
+			}).
+			Value(&secret.Data.LoginPassword.Login),
+		huh.NewInput().
+			Key("password").
+			Title("Password").
+			Validate(func(data string) error {
+				if data == "" {
+					return ErrRequiredField
+				}
+				return nil
+			}).
+			Value(&secret.Data.LoginPassword.Password),
+	}
+	return tuiFormHelper(secret, save, opts)
+}
+
+func tuiFormCredirCard(secret *models.Secret, save *bool) *huh.Form {
+	opts := []huh.Field{
+		huh.NewInput().
+			Title("Card number").
+			Key("card_number").
+			CharLimit(16).
+			Validate(func(data string) error {
+				if data == "" {
+					return ErrRequiredField
+				}
+				if len(data) != 16 {
+					return ErrCardLength
+				}
+				_, err := strconv.ParseInt(data, 10, 64)
+				if err != nil {
+					return ErrOnlyDigits
+				}
+				secret.Data.CreditCard.Number = data
+				return nil
+			}).
+			Value(&secret.Data.CreditCard.Number),
+		huh.NewInput().
+			Title("Exp").
+			Description("MM/YY").
+			Key("card_expire").
+			CharLimit(5).
+			Validate(func(data string) error {
+				if data == "" {
+					return ErrRequiredField
+				}
+
+				if len(data) != 5 {
+					return ErrExpireFormat
+				}
+				parts := strings.Split(data, "/")
+				if len(parts) != 2 {
+					return ErrExpireFormat
+				}
+				i, err := strconv.ParseInt(parts[0], 10, 32)
+				if err != nil {
+					return ErrOnlyDigits
+				}
+				if i < 1 || i > 12 {
+					return ErrCardMonth
+				}
+				_, err = strconv.ParseInt(parts[1], 10, 32)
+				if err != nil {
+					return ErrOnlyDigits
+				}
+
+				secret.Data.CreditCard.Exp = data
+				return nil
+			}).
+			Value(&secret.Data.CreditCard.Exp),
+		huh.NewInput().
+			Key("card_cvv").
+			Title("cvv").
+			CharLimit(3).
+			Validate(func(data string) error {
+				if data == "" {
+					return ErrRequiredField
+				}
+				if len(data) != 3 {
+					return ErrCardCvvLength
+				}
+				_, err := strconv.ParseInt(data, 10, 32)
+				if err != nil {
+					return ErrOnlyDigits
+				}
+				secret.Data.CreditCard.Cvv = data
+				return nil
+			}).
+			Value(&secret.Data.CreditCard.Cvv),
+	}
+	return tuiFormHelper(secret, save, opts)
+}
+
 func tuiFormText(secret *models.Secret, save *bool) *huh.Form {
-	return huh.NewForm(
-		huh.NewGroup(
-			secretNameInput(&secret.Name),
-			huh.NewText().
-				Key("Text").
-				Title("Text").
-				Validate(func(data string) error {
-					if data == "" {
-						return ErrRequiredField
-					}
-					return nil
-				}).
-				Value(&secret.Data.Text),
-			tuiMeta(secret.Meta),
-			tuiComfirm(secret, save),
-		),
-		tuiConfirmDelete(secret, save),
-	)
+	opts := []huh.Field{
+		huh.NewText().
+			Key("Text").
+			Title("Text").
+			Validate(func(data string) error {
+				if data == "" {
+					return ErrRequiredField
+				}
+				return nil
+			}).
+			Value(&secret.Data.Text),
+	}
+	return tuiFormHelper(secret, save, opts)
 }
 
 func tuiFormFile(secret *models.Secret, save *bool, fn func(*models.Secret) (string, error)) *huh.Form {
 	secret.Data.File.Path = ""
 	opts := []huh.Field{
-		secretNameInput(&secret.Name),
 		huh.NewFilePicker().
 			ShowHidden(true). // do not work
 			ShowSize(true).
@@ -258,13 +196,32 @@ func tuiFormFile(secret *models.Secret, save *bool, fn func(*models.Secret) (str
 				}),
 		)
 	}
+	return tuiFormHelper(secret, save, opts)
+}
 
-	opts = append(opts,
+func tuiFormHelper(secret *models.Secret, save *bool, opts []huh.Field) *huh.Form {
+	options := []huh.Field{
+		huh.NewInput().
+			Title("Secret name").
+			Key("name").
+			Validate(func(data string) error {
+				if data == "" {
+					return ErrRequiredField
+				}
+				return nil
+			}).
+			Value(&secret.Name),
+	}
+	options = append(options, opts...)
+	options = append(options,
 		tuiMeta(secret.Meta),
-		tuiComfirm(secret, save).
+		huh.NewConfirm().
+			Title("Save changes?").
+			Key("save").
+			Value(save).
 			Validate(func(b bool) error {
 				if b {
-					if secret.ID == 0 && secret.Data.File.Path == "" {
+					if secret.Type == models.SecretTypeFile && secret.ID == 0 && secret.Data.File.Path == "" {
 						return fmt.Errorf("file: %w", ErrRequiredField)
 					}
 				}
@@ -272,17 +229,40 @@ func tuiFormFile(secret *models.Secret, save *bool, fn func(*models.Secret) (str
 			}),
 	)
 	return huh.NewForm(
-		huh.NewGroup(opts...),
-		tuiConfirmDelete(secret, save),
+		huh.NewGroup(
+			options...,
+		),
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("Delete secret?").
+				Key("delete")).
+			WithHideFunc(func() bool {
+				return secret.ID == 0 || *save
+			}),
 	)
 }
-
-func tuiConfirmDelete(secret *models.Secret, save *bool) *huh.Group {
-	return huh.NewGroup(
-		huh.NewConfirm().
-			Title("Delete secret?").
-			Key("delete")).
-		WithHideFunc(func() bool {
-			return secret.ID == 0 || *save
-		})
+func tuiMeta(meta models.Meta) *huh.Text {
+	metaStr, err := meta.PrettyString()
+	if err != nil {
+		// TODO залогировать ошибку
+	}
+	return huh.NewText().
+		Title("Meta info").
+		Description("json dict format. Key __name__ is not allowed to be used.").
+		Validate(func(data string) error {
+			clear(meta)
+			if data == "" {
+				return nil
+			}
+			err := json.Unmarshal([]byte(data), &meta)
+			if err != nil {
+				return fmt.Errorf("%w: %w", ErrNotDict, err)
+			}
+			if _, ok := meta[models.MetaKeyName]; ok {
+				return ErrMetaName
+			}
+			return nil
+		}).
+		Value(&metaStr).
+		Key("meta")
 }
