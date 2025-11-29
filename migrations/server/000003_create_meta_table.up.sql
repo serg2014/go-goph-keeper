ffe22e3b87ad447359b0b707da2a508ea5ddae4a
Serg K <kozunov@example.com>
2025-11-30 00:45:35 +0300

    add server tables

---
+++ migrations/server/000003_create_meta_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS meta (
+    id BIGSERIAL PRIMARY KEY,
+    user_id uuid NOT NULL,
+    secret_id bigint NOT NULL,
+    version integer NOT NULL DEFAULT 0,
+    updated_at integer NOT NULL,
+    data bytea NOT NULL
+);
+CREATE UNIQUE INDEX meta_user_id_secret_id_idx ON meta USING btree (user_id, secret_id);
---
+++ migrations/server/000004_create_data_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS data (
+    id BIGSERIAL PRIMARY KEY,
+    user_id uuid NOT NULL,
+    secret_id bigint NOT NULL,
+    version integer NOT NULL DEFAULT 0,
+    updated_at integer NOT NULL,
+    data bytea NOT NULL
+);
+CREATE UNIQUE INDEX data_user_id_secret_id_idx ON data USING btree (user_id, secret_id);
